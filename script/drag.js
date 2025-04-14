9b37087439f1bad5fbe04599f008cc74855287fc
Markel Cortajarena <markelkortajarena@gmail.com>
2025-04-14 16:11:23 +0200

    Implement drag-and-drop functionality for task elements within swim lanes

--- script/drag.js
+++ script/drag.js
@@ -1,4 +1,5 @@
 const draggables = document.querySelectorAll(".task");
+const droppables = document.querySelectorAll(".swim-lanes");
 
 draggables.forEach((task) => {
     task.addEventListener("dragstart", () => {
@@ -8,3 +9,38 @@ draggables.forEach((task) => {
       task.classList.remove("is-dragging");
     });
 });
+
+droppables.forEach((zone) => {
+    zone.addEventListener("dragover", (e) => {
+      e.preventDefault();
+  
+      const bottomTask = insertAboveTask(zone, e.clientY);
+      const curTask = document.querySelector(".is-dragging");
+  
+      if (!bottomTask) {
+        zone.appendChild(curTask);
+      } else {
+        zone.insertBefore(curTask, bottomTask);
+      }
+    });
+});
+
+const insertAboveTask = (zone, mouseY) => {
+    const els = zone.querySelectorAll(".task:not(.is-dragging)");
+
+    let closestTask = null;
+    let closestOffset = Number.NEGATIVE_INFINITY;
+
+    els.forEach((task) => {
+        const { top } = task.getBoundingClientRect();
+
+        const offset = mouseY - top;
+
+        if (offset < 0 && offset > closestOffset) {
+        closestOffset = offset;
+        closestTask = task;
+        }
+    });
+
+    return closestTask;
+};
